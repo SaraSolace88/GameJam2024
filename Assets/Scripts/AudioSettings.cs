4f8374fb2f41a08e948a89bc7c048a8a4cbe61ac
Tostahh <114684478+Tostahh@users.noreply.github.com>
2024-01-26 20:18:39 -0600

    All Settings are in

---
+++ Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer MainMixer;
+    [SerializeField] private Slider MasterVolumeSlider;
+    [SerializeField] private Slider MusicVolumeSlider;
+    [SerializeField] private Slider EffectsVolumeSlider;
+    private void Start()
+    {
+        if(PlayerPrefs.HasKey("MVolume") || PlayerPrefs.HasKey("MuVolume") || PlayerPrefs.HasKey("EfVolume"))
+        {
+            LoadMasterVolume();
+            LoadMusicVolume();
+            LoadEFXVolume();
+        }
+        else
+        {
+            SetMasterVolume();
+            SetMusicVolume();
+            SetEFXVolume();
+        }
+    }
+
+    public void SetMasterVolume()
+    {
+        float MasterVolume = MasterVolumeSlider.value;
+        MainMixer.SetFloat("MasterVolume", MasterVolume);
+        PlayerPrefs.SetFloat("MVolume", MasterVolume);
+    }
+    public void SetMusicVolume()
+    {
+        float MusicVolume = MusicVolumeSlider.value;
+        MainMixer.SetFloat("MusicVolume", MusicVolume);
+        PlayerPrefs.SetFloat("MuVolume", MusicVolume);
+    }
+    public void SetEFXVolume()
+    {
+        float EffectsVolume = EffectsVolumeSlider.value;
+        MainMixer.SetFloat("EffectsVolume", EffectsVolume);
+        PlayerPrefs.SetFloat("EfVolume", EffectsVolume);
+    }
+
+    private void LoadMasterVolume()
+    {
+        MasterVolumeSlider.value = PlayerPrefs.GetFloat("MVolume");
+        SetMasterVolume();
+    }
+    private void LoadMusicVolume()
+    {
+        MusicVolumeSlider.value = PlayerPrefs.GetFloat("MuVolume");
+        SetMusicVolume();
+    }
+    private void LoadEFXVolume()
+    {
+        EffectsVolumeSlider.value = PlayerPrefs.GetFloat("EfVolume");
+        SetMusicVolume();
+    }
+
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject PausePanel;
+    [SerializeField] private GameObject SettingsPanel;
+
+    public bool IsPaused;
+    public Toggle FullScreen;
+    public Toggle Vsync;
+
+    public List<ResolutionItem> Resolutions = new List<ResolutionItem>();
+    private int SelectedRes;
+    public TextMeshProUGUI ResSettingDisplay;
+
+    private void Start()
+    {
+        FullScreen.isOn = Screen.fullScreen;
+        if (QualitySettings.vSyncCount == 0)
+        {
+            Vsync.isOn = false;
+        }
+        else
+        {
+            Vsync.isOn = true;
+        }
+
+        bool foundRes = false;
+        for (int i = 0; i < Resolutions.Count; i++)
+        {
+            if (Screen.width == Resolutions[i].Horizontal && Screen.height == Resolutions[i].Vertical)
+            {
+                foundRes = true;
+
+                SelectedRes = i;
+            }
+        }
+
+        if (!foundRes)
+        {
+            ResolutionItem newRes = new ResolutionItem();
+            newRes.Horizontal = Screen.width;
+            newRes.Vertical = Screen.height;
+            Resolutions.Add(newRes);
+            SelectedRes = Resolutions.Count - 1;
+        }
+    }
+    private void OnGUI()
+    {
+        ResSettingDisplay.text = Resolutions[SelectedRes].Horizontal.ToString() + " X " + Resolutions[SelectedRes].Vertical.ToString();
+    }
+    public void Pause()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+    public void Resume()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+    public void OpenSettings()
+    {
+        SettingsPanel.SetActive(true);
+    }
+    public void CloseSettings()
+    {
+        SettingsPanel.SetActive(false);
+    }
+    public void ResLeft()
+    {
+        SelectedRes--;
+        if (SelectedRes < 0)
+        {
+            SelectedRes = 0;
+        }
+    }
+    public void ResRight()
+    {
+        SelectedRes++;
+        if (SelectedRes > Resolutions.Count - 1)
+        {
+            SelectedRes = Resolutions.Count - 1;
+        }
+    }
+    public void ApplyGraphics()
+    {
+        if (Vsync.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+
+        Screen.SetResolution(Resolutions[SelectedRes].Horizontal, Resolutions[SelectedRes].Vertical, FullScreen.isOn);
+    }
+}
+
+[System.Serializable]
+public class ResolutionItem
+{
+    public int Horizontal;
+    public int Vertical;
+}
